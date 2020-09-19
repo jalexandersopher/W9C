7aad11658d1cced921da7e9a06692f83e063ab8e
jalexandersopher <jalexandersopher@gmail.com>
2020-09-19 11:34:06 -0600

    W9E finished

--- js/app.js
+++ js/app.js
@@ -21,13 +21,71 @@ var tweets = [
         age: 28,
         created_at: "05-17-2020"
     },
+    {
+        tweet: "tweet 4",
+        username: "Dave",
+        age: 24,
+        created_at: "05-15-2020"
+    },
+
+    {
+        tweet: "tweet 5",
+        username: "Lisa",
+        age: 19,
+        created_at: "05-16-2020"
+    },
+
+    {
+        tweet: "tweet 6",
+        username: "Beth",
+        age: 16,
+        created_at: "05-17-2020"
+    },
+    {
+        tweet: "tweet 7",
+        username: "Wes",
+        age: 27,
+        created_at: "05-15-2020"
+    },
+
+    {
+        tweet: "tweet 8",
+        username: "Matt",
+        age: 45,
+        created_at: "05-16-2020"
+    },
+
+    {
+        tweet: "tweet 9",
+        username: "Millie",
+        age: 29,
+        created_at: "05-17-2020"
+    },
+    {
+        tweet: "tweet 10",
+        username: "Tom",
+        age: 14,
+        created_at: "05-15-2020"
+    },
+
+    {
+        tweet: "tweet 11",
+        username: "Mark",
+        age: 50,
+        created_at: "05-16-2020"
+    },
+
+    {
+        tweet: "tweet 12",
+        username: "Sasha",
+        age: 17,
+        created_at: "05-17-2020"
+    },
 ];
 
+function ageCheck(tweets) {
+    return tweets.age >= 18;
+}
 
-
-for(var index = 0; index < tweets.length; index++) {
-    console.log(tweets[index].tweet);
-    console.log(tweets[index].username);
-    console.log(tweets[index].created_at);
-    console.log("-----------------------");
-};
+var overEighteentweets = tweets.filter(ageCheck);
+console.log(overEighteentweets);
